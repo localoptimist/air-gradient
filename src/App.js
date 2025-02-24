4e5b7c9b8ca77d09080282e64c57b3876c5bbe74
localoptimist <francis@pollaras.com>
2025-02-23 20:05:04 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// src/App.js
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Dashboard />
+    </div>
+  );
+}
